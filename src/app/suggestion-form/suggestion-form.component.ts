50abe01c771edc4c00fa5258649a3b3d607641d9
Valeria Castillo <valeriacastillo415@gmail.com>
2020-05-01 17:35:09 -0700

    stash

--- src/app/suggestion-form/suggestion-form.component.ts
+++ src/app/suggestion-form/suggestion-form.component.ts
@@ -1,44 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { ItemService } from '../services/item.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-
-
+import { Component, OnInit } from "@angular/core";
+import { ItemService } from "../services/item.service";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-suggestion-form',
-  templateUrl: './suggestion-form.component.html',
-  styleUrls: ['./suggestion-form.component.css']
+  selector: "app-suggestion-form",
+  templateUrl: "./suggestion-form.component.html",
+  styleUrls: ["./suggestion-form.component.css"],
 })
-
 export class SuggestionFormComponent implements OnInit {
-newIdeas = []; // ideas array
-votes: number;
-showSubmit = false;
-successMsg = false;
+  newIdeas = []; // ideas array
+  votes: number;
+  showSubmit = false;
+  successMsg = false;
 
   // create an object of the class inside the constructor
-constructor(public itemService: ItemService) { }
+  constructor(public itemService: ItemService) {}
 
   // adds idea
-addIdea = idea => {
+  addIdea = (idea) => {
     this.newIdeas.push(idea); // adds new idea to array
-
-  }
+  };
 
   // remove idea
-removeIdea = idea => {
+  removeIdea = (idea) => {
     // returns the index within the calling this.coffeeOrder object of the first occurrence
     // of the specified value, starting the search at -1 if the value is not found.
     // declaring the index
     const index = this.newIdeas.indexOf(idea);
     // changes the content of an array, adding new elements while removing old elements.
-    if (index > -1) { this.newIdeas.splice(index, 1); }
-  }
-
-
+    if (index > -1) {
+      this.newIdeas.splice(index, 1);
+    }
+  };
 
   // add an great idea
-onSubmit() {
+  onSubmit() {
     // map newIdeas array to the form value idea
     // this.itemService.form.value.newIdea = this.newIdeas;
 
@@ -51,24 +47,25 @@ onSubmit() {
     // create idea
     this.itemService.createNewIdea(data);
 
-
     // resets form
     this.itemService.form.reset();
 
-    console.log('New idea has been submitted!');
+    console.log("New idea has been submitted!");
 
     // reset form to pristine conditions
     this.itemService.form.reset();
 
-}
+    this.successMsg = true;
+  }
 
-
-ngOnInit() {
-      // validation
-      this.itemService.form = new FormGroup({
-        newIdea: new FormControl('', [Validators.required, Validators.minLength(2)]),
-        votes: new FormControl()
-      });
-}
-
+  ngOnInit() {
+    // validation
+    this.itemService.form = new FormGroup({
+      newIdea: new FormControl("", [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
+      votes: new FormControl(),
+    });
+  }
 }
